ec79cd830a6795116e8b0f2199befdecb767d17b
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2024-11-06 01:12:05 +0100

    Update DynamicSampling.jl

--- src/DynamicSampling.jl
+++ src/DynamicSampling.jl
@@ -8,28 +8,24 @@ using Random
 struct DynamicSampler{R}
     rng::R
     N::Int
-    toplevel::Int
     totweight::Base.RefValue{Float64}
     weights::Vector{Float64}
     level_weights::Vector{Float64}
     level_buckets::Vector{Vector{Int}}
     level_max::Vector{Float64}
+    level_inds::Vector{Int}
 end
 
-function DynamicSampler(N::Int, minweight=1.0, maxweight=100.0)
-	return DynamicSampler(Random.default_rng(), N, minweight, maxweight)
-end
-function DynamicSampler(rng, N::Int, minweight=1.0, maxweight=100.0)
-    toplevel = ceil(Int, log2(maxweight))
-    bottomlevel = ceil(Int, log2(minweight))
-    nlevels = toplevel - bottomlevel + 1
+DynamicSampler(N::Int) = DynamicSampler(Random.default_rng(), N)
+function DynamicSampler(rng, N::Int)
     totweight = 0.0
     weights = zeros(Float64, N)
-    level_weights = zeros(Float64, nlevels)
-    level_buckets = [Int[] for _ in 1:nlevels]
-    level_max = [2.0^(toplevel-i+1) for i in 1:nlevels]
-    return DynamicSampler(rng, N, toplevel, Ref(totweight), weights,
-        level_weights, level_buckets, level_max)
+    level_weights = Float64[0.0]
+    level_buckets = [Int[],]
+    level_max = Float64[]
+    level_inds = Int[]
+    return DynamicSampler(rng, N, Ref(totweight), weights, level_weights, 
+    	level_buckets, level_max, level_inds)
 end
 
 struct DynamicIndex
@@ -42,8 +38,11 @@ DynamicIndex(idx, weight) = DynamicIndex(idx, weight, 0, 0)
 
 function Base.push!(S::DynamicSampler, e::Tuple)
     idx, weight = e
+    S.weights[idx] != 0.0 && error()
     S.totweight[] += weight
-    level = getlevel(S.toplevel, weight)
+    level_raw = ceil(Int, log2(weight))
+    createlevel!(S, level_raw)
+    level = getlevel(first(S.level_inds), weight)
     S.level_weights[level] += weight
     push!(S.level_buckets[level], idx)
 	S.weights[idx] = weight
@@ -54,14 +53,17 @@ function Base.append!(S::DynamicSampler, e::Tuple)
     inds, weights = e
     nlevels = zeros(Int, length(S.level_buckets))
     sumweights = 0.0
-    levs = zeros(Int8, length(inds))
+    levs = zeros(Int16, length(inds))
     for (i, w) in enumerate(weights)
-        level = getlevel(S.toplevel, w)
+        S.weights[i] != 0.0 && error()
+        level_raw = ceil(Int, log2(w))
+        createlevel!(S, level_raw, nlevels)
+        level = level_raw - first(S.level_inds) + 1
         nlevels[level] += 1
         S.level_weights[level] += w
         S.weights[i] = w
         sumweights += w
-        levs[i] = level
+        levs[i] = level_raw
     end
     S.totweight[] += sumweights
     for (i, bucket) in enumerate(S.level_buckets)
@@ -69,7 +71,8 @@ function Base.append!(S::DynamicSampler, e::Tuple)
         nlevels[i] = length(bucket)
     end
     for (i, id) in enumerate(inds)
-        level = levs[i]
+        level_raw = levs[i]
+        level = level_raw - first(S.level_inds) + 1
         bucket = S.level_buckets[level]
         bucket[nlevels[level]] = i
         nlevels[level] -= 1
@@ -90,6 +93,12 @@ function sample(S::DynamicSampler; info = false)
     # Now sample within the level using rejection sampling
     bucket = S.level_buckets[level]
     level_size = length(bucket)
+    if level_size == 0
+        notempty = findall(b -> length(b) > 0, S.level_buckets)
+        level = rand(S.rng, notempty)
+        bucket = S.level_buckets[level]
+        level_size = length(bucket)
+    end
     level_max = S.level_max[level]
     while true
         idx_in_level = rand(S.rng, 1:level_size)
@@ -102,7 +111,7 @@ end
     
 function Base.deleteat!(S::DynamicSampler, idx)
     weight = S.weights[idx]
-    level = getlevel(S.toplevel, weight)
+    level = getlevel(first(S.level_inds), weight)
     idx_in_level = findfirst(x -> x == idx, S.level_buckets[level])
     isnothing(idx_in_level) && return error()
     _deleteat!(S, idx, weight, level, idx_in_level)
@@ -123,6 +132,30 @@ function _deleteat!(S, idx, weight, level, idx_in_level)
     pop!(bucket)
 end
 
-getlevel(toplevel, weight) = toplevel - ceil(Int, log2(weight)) + 1
+function createlevel!(S, level_w, nlevels=nothing)
+    if isempty(S.level_max)
+        push!(S.level_max, 2.0^level_w)
+        push!(S.level_inds, level_w)
+    elseif level_w > S.level_inds[end]
+        for i in S.level_inds[end]+1:level_w
+            push!(S.level_max, 2.0^i)
+            push!(S.level_inds, i)
+            push!(S.level_buckets, Int[])
+            push!(S.level_weights, 0.0)
+            !isnothing(nlevels) && push!(nlevels, 0)
+        end
+    elseif level_w < S.level_inds[begin]
+        for i in S.level_inds[begin]-1:-1:level_w-1
+            pushfirst!(S.level_max, 2.0^i)
+            pushfirst!(S.level_inds, i)
+            pushfirst!(S.level_buckets, Int[])
+            pushfirst!(S.level_weights, 0.0)
+            !isnothing(nlevels) && pushfirst!(nlevels, 0)
+        end
+    end
+    return S
+end
+
+getlevel(minlevel, weight) = ceil(Int, log2(weight)) - minlevel + 1
 
 end
