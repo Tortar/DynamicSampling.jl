f1d1cc115b3fa5aea007eedb216f82b384a7b1e6
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2024-11-09 19:40:58 +0100

    Increase tests (#2)

--- test/weighted_sampler_tests.jl
+++ test/weighted_sampler_tests.jl
@@ -53,4 +53,24 @@
 
     chisq_test = ChisqTest(counts_est, ps_exact)
     @test pvalue(chisq_test) > 0.05
+
+    s4 = DynamicSampler(rng)
+    
+    for (i, w) in zip(range, weights2)
+        push!(s4, (i, w))
+    end
+
+    deleteat!(s4, 1)
+    deleteat!(s4, 2)
+
+    push!(s4, (2, 200.0))
+    push!(s4, (1000, 1000.0))
+
+    samples_counts = countmap([rand(s4) for _ in 1:10^5])
+    counts_est = [samples_counts[i] for i in [2:b..., 1000]]
+    wsum = (b ÷ 2)*(b+1) - 3 + 200 + 1000
+    ps_exact = [i == 2 ? 200/wsum : i/wsum for i in [2:b..., 1000]]
+
+    chisq_test = ChisqTest(counts_est, ps_exact)
+    @test pvalue(chisq_test) > 0.05
 end
