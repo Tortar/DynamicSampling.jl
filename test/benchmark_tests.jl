ac2227d3253022e9a41f29bf9790d43910a0e6e1
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2024-11-11 23:44:45 +0100

    Update benchmark_tests.jl

--- test/benchmark_tests.jl
+++ test/benchmark_tests.jl
@@ -1,10 +1,10 @@
 
 using BenchmarkTools
 
-function b(rng, n)
+function b1(rng, n)
     s = DynamicSampler(rng)
     sizehint!(s, n)
-    append!(s, (1:n, (rand(rng) for _ in 1:n)))
+    append!(s, 1:n, (rand(rng) for _ in 1:n))
     t = 0
     for i in 1:n-1
         e = rand(s; info=true)
@@ -14,12 +14,31 @@ function b(rng, n)
     return t
 end
 
+function b2(rng, n)
+    s = DynamicSampler(rng)
+    sizehint!(s, n)
+    for i in 1:n
+        push!(s, i, rand(rng))
+    end
+    t = 0
+    for i in 1:n-1
+        idx = rand(s)
+        t += idx
+        deleteat!(s, idx)
+    end
+    return t
+end
+
 @testset "Benchmark Tests" begin
     rng = Xoshiro(42)
-    b_small = @benchmark b($rng, 100)
-    b_large = @benchmark b($rng, 10^6)
-
+    b_small_fast = @benchmark b1($rng, 100)
+    b_large_fast = @benchmark b1($rng, 10^6)
+    b_small_slow = @benchmark b2($rng, 100)
+    b_large_slow = @benchmark b2($rng, 10^6)
+    
     println("Benchmark results\n")
-    println("Benchmark 10^2 items: $(mean(b_small.times)/10^6) ms")
-    println("Benchmark 10^6 items: $(mean(b_large.times)/10^6) ms")
+    println("Benchmark Fast 10^2 items: $(mean(b_small_fast.times)/10^6) ms")
+    println("Benchmark Fast 10^6 items: $(mean(b_large_fast.times)/10^6) ms")
+    println("Benchmark Slow 10^2 items: $(mean(b_small_slow.times)/10^6) ms")
+    println("Benchmark Slow 10^6 items: $(mean(b_large_slow.times)/10^6) ms")
 end
