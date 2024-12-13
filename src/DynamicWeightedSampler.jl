bbaa0b1f101d9b9d66bda59135af9c505a7b4724
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2024-12-13 21:12:57 +0100

    Remove allocations in researching levels (#28)

--- src/DynamicWeightedSampler.jl
+++ src/DynamicWeightedSampler.jl
@@ -184,8 +184,9 @@ end
         sortperm!(sp.order_level, sp.level_weights)
         n_notempty = sum(length(b) > 0 for b in sp.level_buckets)
         rand_notempty = rand(sp.rng, 1:n_notempty)
-        notempty = Iterators.filter(x -> length(x[2]) > 0, enumerate(sp.level_buckets))
-        level, bucket = first(Iterators.drop(notempty, rand_notempty-1))
+        notempty = Iterators.filter(i -> !isempty(sp.level_buckets[i]), eachindex(sp.level_buckets))
+        level = first(Iterators.drop(notempty, rand_notempty-1))
+        bucket = sp.level_buckets[level]
     end
     return level, bucket
 end
@@ -327,7 +328,7 @@ end
     n1, n2 = length(sp.level_weights), length(sp.order_level)
     if n2 < n1
         resize!(sp.order_level, n1)
-        for i in (n2+1):n1
+        @inbounds @simd for i in (n2+1):n1
             sp.order_level[i] = i
         end
     end
